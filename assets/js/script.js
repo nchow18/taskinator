47f07cfdd462f5966fa45a8ff8cc035865d4f2c4
Nathan Chow <emailme@nathanchow.ca>
2020-12-23 12:18:48 -0500

    update dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,14 @@
 
-// create a save task variable called buttonEl, this will select the item "save-task" in html, within Button 
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-// select the ID in button to perform the task
-var buttonEl = document.querySelector("#save-task"); 
-// variable for where the new element will be saved in "main"
+var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-// create a function with a condition
-//variable for where to create the element
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-var createTaskHandler = function() { 
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
